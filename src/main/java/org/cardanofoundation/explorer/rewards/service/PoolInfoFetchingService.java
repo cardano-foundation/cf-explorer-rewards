66964e3172bbe90839804178a5736d62296dcd28
sotatek-huyle3 <huy.le3@sotatek.com>
2023-06-08 15:53:34 +0700

    fix: ADAE-499 Delegator and Saturation are not stable

--- src/main/java/org/cardanofoundation/explorer/rewards/service/PoolInfoFetchingService.java
+++ src/main/java/org/cardanofoundation/explorer/rewards/service/PoolInfoFetchingService.java
@@ -6,6 +6,6 @@ import java.util.concurrent.CompletableFuture;
 import rest.koios.client.backend.api.base.exception.ApiException;
 
 public interface PoolInfoFetchingService {
+
   CompletableFuture<Boolean> fetchData(List<String> poolIds) throws ApiException;
-  List<String> getPoolIdListNeedFetchData(List<String> poolIds);
 }
